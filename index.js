f7b66ba995486f1eb4fb4a8690c7a7d8d3c70bb4
Jordan  Haddadi <jordanhaddadi@gmail.com>
2018-05-22 22:20:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+var a = 3; 
+var b = 9;
+
+function add(a, b) {
+    return a + b; 
+}
+function subtract(b, a) {
+    return b - a; 
+}
+function multiply(a, b) {
+    return a * b; 
+}
+function divide(b, a) {
+    return b / a; 
+}
+
+var number = 5;
+
+function inc(number) {
+  return number += 1; 
+}
+
+function dec(number) {
+  return number -= 1; 
+}
+
+function makeInt(n) {
+  var parsed = parseInt('2', 10);
+}
+
+function makeInt(n) {
+  var parsed = parseInt('2.2222', 10);
+  return n; 
+}
